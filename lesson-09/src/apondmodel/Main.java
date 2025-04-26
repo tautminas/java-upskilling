7b7ecb7270c9ce6f321671f98c64a39b54e99b8b
Tautminas Cibulskis <tautminas@yahoo.com>
2025-04-26 22:01:25 +0300

    Add missing task condition

--- lesson-09/src/apondmodel/Main.java
+++ lesson-09/src/apondmodel/Main.java
@@ -4,6 +4,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
+
+    /*
+     2. A pond model
+     Create a pond model with stones, weeds and fishes. A Stone only has coordinates (x , y) and stay
+     at its place indefinitely. A Weed extends Stone and can also grow and die. A Fish extends Weed
+     and can also move. Create a couple of objects of each class and a loop for the lifecycle of the
+     living objects. Make sure a Fish does not move to a place where there is already a weed or a
+     stone.
+     You may want to use an instanceof operator for completing the task:
+     https://www.w3schools.com/java/ref_keyword_instanceof.asp
+     Those, who want an optional extra challenge, may create a 2-dimensional array and simulate a
+     real pond ecosystem with growing weed and fish, which is eating weed. May even add big fishes,
+     which eat small fishes.
+     https://saask.ee/model
+    */
+
     public static void main(String[] args) throws InterruptedException {
         int pondWidth = 10;
         int pondHeight = 10;
